b2f407914f3eab7b5ab95a54ebe8d457a33db277
Craig <cav9871@saintjoe.edu>
2015-06-20 15:41:54 -0400

    I created a custome directive to output the questions from the database instead of having the code on the index.html page

--- angularSPA/controllers.js
+++ angularSPA/controllers.js
@@ -7,8 +7,23 @@ myApp.controller('AppCtrl',['$scope','$http',function($scope,$http) {
             console.log("got data");
             $scope.multipleChoice = response;
             $scope.question = "";
+            
         });
+    
+        
+    
+
+
+    
+    
 }]);
+
+myApp.directive('questionGroup', function() {
+    return {
+        template: '<div ng-repeat="question in multipleChoice"> <div> {{$index + 1}}) {{question.text}} <div ng-repeat="choice in question.decoys | limitTo:2" class="alert" ><label><input value="{{choice}}" type="radio" name="{{$index}}"/>{{choice}}</label></div></div>  </div>'
+                }
+    
+});
 
 
 
