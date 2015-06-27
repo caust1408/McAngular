1c2d6dade428d9ed099f14e240f05df8d082ea28
Craig <cav9871@saintjoe.edu>
2015-06-27 00:50:38 -0400

    I added in the database for the match section of the test

--- choice.js
+++ choice.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('multipleChoice', ['multipleChoice']);
+var db = mongojs('multipleChoice', ['multipleChoice','match']);
+
 var bodyParser = require('body-parser');
 
 
@@ -9,9 +10,16 @@ app.use(express.static(__dirname + "/angularSPA"));
 app.use(bodyParser.json());
 
 app.get('/multipleChoice', function(req, res) {
-    console.log("I got request");
-    
+    //console.log(res);
     db.multipleChoice.find(function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+app.get('/match', function(req, res) {
+    //console.log(res);
+    db.match.find(function(err, docs) {
         console.log(docs);
         res.json(docs);
     });
